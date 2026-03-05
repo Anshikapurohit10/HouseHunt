f3ce5ff3b85a5104651a6cafd35eaedde0ebe216
Anshikapurohit10 <anshikapurohit4@gmail.com>
2026-03-05 22:00:25 +0530

    Update LoginPage.jsx

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -16,7 +16,7 @@ const LoginPage = () => {
     e.preventDefault()
 
     try {
-      const response = await fetch ("http://localhost:3001/auth/login", {
+      const response = await fetch ("https://househunt-8vj2.onrender.com/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
